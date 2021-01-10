71038f098e79bdf855a90bb1b6b6335aa737f53d
Sharp0802 <nrs070802@gmail.com>
2021-01-10 15:40:02 +0900

    Revert "Convert funcs for serializer"
    This reverts commit 3e4471361a7588f2dc486c25e2d636407d4f5cd0.

--- XmlSharp/Serialize.cs
+++ XmlSharp/Serialize.cs
@@ -2,8 +2,6 @@
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
 using System.Xml;
 
 namespace XmlSharp
@@ -78,29 +76,6 @@ namespace XmlSharp
         public static string Deserialize(this string path)
         {
             return File.ReadAllText(path);
-        }
-    }
-
-    internal static class Convert
-    {
-        internal static string BytesToString(byte[] bytes) => Encoding.UTF8.GetString(bytes);
-        internal static byte[] StringToBytes(string str) => Encoding.UTF8.GetBytes(str);
-
-        public static object BytesToObject(byte[] arrBytes)
-        {
-            using var memStream = new MemoryStream();
-                var binForm = new BinaryFormatter();
-                memStream.Write(arrBytes, 0, arrBytes.Length);
-                memStream.Seek(0, SeekOrigin.Begin);
-                var obj = binForm.Deserialize(memStream);
-                return obj;
-        }
-        public static byte[] ObjectToBytes(object obj)
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            using var ms = new MemoryStream();
-                bf.Serialize(ms, obj);
-                return ms.ToArray();
         }
     }
 }
